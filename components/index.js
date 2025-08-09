5e328694f00a6ce30d3c4914474a7d290b95fce0
Erling Hauan <erling.hauan@proton.me>
2025-08-09 08:05:04 +0200

    remove unused component BlogMain

--- components/index.js
+++ components/index.js
@@ -1,11 +1,9 @@
 import { BlogArchive } from "./BlogArchive.js";
 import { BlogEntry } from "./BlogEntry.js";
 import { BlogHeader } from "./BlogHeader.js"
-import { BlogMain } from "./BlogMain.js";
 import { BlogThemeToggle } from "./BlogThemeToggle.js";
 
 customElements.define("blog-header", BlogHeader);
-customElements.define("blog-main", BlogMain);
 customElements.define("blog-archive", BlogArchive);
 customElements.define("blog-theme-switch", BlogThemeToggle);
 customElements.define("blog-entry", BlogEntry);
