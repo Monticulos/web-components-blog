537de18dba95d8790473cf9ffe2ea654f6d836e2
Erling Hauan <erling.hauan@proton.me>
2025-08-09 07:06:21 +0200

    update first entry

---
+++ entries/2025-08-09.js
@@ -0,0 +1,18 @@
+const title = "Velkommen til bloggen"
+
+const bodyText = `
+<p>Velkommen til Bloggen - et prosjekt laget med Web Components. Den har to sider, visning av blogginnlegg og arkivet. I fremtiden kommer jeg også til å lage en egen side med emner.</p>
+
+<p>Denne bloggen oppstod av flere årsaker. Jeg har en kollega som snakket om Web Components og sa at det hadde kommet ganske langt. Da ble jeg nysgjerrig på hva det var for noe, og hvordan det er å bruke i forhold til React. Jeg er også interessert i hva rammeverk som React gjør for oss, og da er det greit å se hvordan ting fungerer uten rammeverk. I tillegg hadde vi en episode på jobb, da en NPM-avhengighet ble flagget som ondsinnet kode og tatt ned midlertidig, som gjorde meg interessert i å prøve å bygge noe uten avhengigheter.</p>
+`
+
+export default {
+    title,
+    publishedDate: "9. august 2025",
+    publishedTime: "07.04",
+    lastUpdatedDate: "",
+    lastUpdatedTime: "",
+    slugs: ["velkommen-til-bloggen", "velkommen"],
+    tags: ["Bloggen", "Programmering"],
+    bodyText
+}
--- entries/index.js
+++ entries/index.js
@@ -1,4 +1,4 @@
 // Add new entries here (oldest first)
 export const entries = [
-    '2025-08-08',
+    '2025-08-09',
 ];
