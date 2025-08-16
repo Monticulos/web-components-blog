bae36ab30e96635662c4d211f394bd972255b216
Erling <131489541+Monticulos@users.noreply.github.com>
2025-08-17 00:01:52 +0200

    fix: flashes with wrong background color when changing page

--- todo.md
+++ todo.md
@@ -1,5 +1,4 @@
 # To do
-* Forhindre flickering ved bytte mellom index.html og archive.html
 * Lage byggesteg for å populere entries-arrayet i index.js? 
 * Lage byggesteg slik at hver side kan få et eget html-dokument (SEO)?
 * Søkeboks på arkiv-siden
---
+++ utils/retainThemeWhileLoading.js
@@ -0,0 +1,11 @@
+// Prevents flashes with wrong background color when loading page
+(function retainThemeWhileLoading() {
+    const storedPreference = localStorage.getItem("theme-preference");
+    if (storedPreference) {
+        document.body.classList.add(storedPreference);
+    } else {
+        const hasDarkPreference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        const themePreference = hasDarkPreference ? "dark" : "light";
+        document.body.classList.add(themePreference);
+    }
+}());
