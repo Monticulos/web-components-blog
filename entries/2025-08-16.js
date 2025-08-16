86e39c98ed139eed16c88897f565788b9c0ebd6c
Erling <131489541+Monticulos@users.noreply.github.com>
2025-08-16 12:57:15 +0200

    content: De tre klassiske overbevisningsformene

---
+++ entries/2025-08-16.js
@@ -0,0 +1,62 @@
+const title = "De tre klassiske overbevisningsformene"
+
+const bodyText = `
+<p>Det er til stadighet noen som forsøker å overtale oss. Det kan være en kollega som vil at du skal gjøre noe for dem, reklameannonser i feeden din, eller noen du bor med som begrunner hvorfor de ikke har gjort sin del av husarbeidet. Vi rettferdiggjør også våre egne handlinger når vi tenker tilbake til dem, i en form for indre overtalelse. Siden påvirkningsforsøkene kommer på løpende bånd, er det verdt å bli bedre kjent med deres form og virkning.</p>
+
+<p>Retorikken - kunsten å overbevise - har røtter tilbake til Aristoteles, som levde i Hellas i det fjerde århundret før vår tidsregning. I moderne tid brukes hans lære som grunnlag i retorikkemner på skoler og universiteter. Han beskrev de tre retoriske virkemidlene <i>etos</i>, <i>logos</i> og <i>patos</i>, som berører henholdsvis karakter, fornuft og følelser.</p>
+
+<h2>Etos - appell til troverdighet</h2>
+
+<p>Ordet <i>etos</i> kommer fra gresk, og kan oversettes direkte til “karakter”, og handler om talerens troverdighet og autoritet. Det dreier seg om å overbevise lytteren om hvorfor hun skal høre på akkurat deg.</p>
+
+<p>For å styrke sin etos kan taleren nevne kvalifikasjoner som relevant utdanning og erfaring. Riktig talemåte og antrekk med hensyn til publikumet er også med på å styrke troverdigheten.</p>
+
+<p>Eksempler på etos:
+    <ul>
+        <li>En politiker som nevner hvilke bedrifter hun har besøkt, for å vise til god kontakt med folket</li>
+        <li>En produsent av eksklusive biler som bruker kjente sportsutøvere i reklamen sin, for å bygge en assosiasjon mellom utøverens karakter og bilmerket</li>
+        <li>En dresskledd revisor med masterutdanning, 20 års arbeidserfaring og tre bokutgivelser vil ha sterk etos på en økonomikonferanse, men samme person vil kanskje ikke stå like sterkt på et avslappet surfetreff</li>
+    </ul>
+</p>
+
+<h2>Logos - appell til fornuft</h2>
+
+<p>Det greske ordet <i>logos</i> kan oversettes direkte til “fornuft”, og er nært beslektet med ordet “logikk”. Denne overbevisningsformen handler om å oppgi fakta og logiske resonnementer som underbygger saken. Ved å vise til sterke, konkrete bevis for et synspunkt, vil argumentasjonen bli mer overbevisende. </p>
+
+<p>Eksempler på logos:
+    <ul>
+        <li>En bedriftsleder viser til svake årsresultater for å rettferdiggjøre en oppsigelse</li>
+        <li>En klimaaktivist peker på sammenhengen mellom drivhusgasser og temperaturøkning, og utleder at vi må bremse utslippene for å bremse temperaturøkning</li>
+        <li>En politiker viser til et naboland som innførte en utdanningsreform som ledet til forbedrede skoleresultater, og at vi derfor kan forvente lignende resultater</li>
+    </ul>
+</p>
+
+<h2>Etos - appell til følelser</h2>
+
+<p>Ordet <i>patos</i> kommer, som du sikkert har gjettet, også fra gresk. Patos kan oversettes til “følelse” og er beslektet med ord som “sympati” og “empati”. Denne overbevisningsformen har som mål å påvirke tilhørerne emosjonelt. Vi mennesker påvirkes ofte mer av følelser enn fakta, derfor er dette en verdifull overbevisningsform å kjenne til.</p>
+
+<p>Eksempler på patos:
+    <ul>
+        <li>En politiker råder til å ikke stemme på sin motstander, fordi vi vil gå en skremmende fremtid i møte</li>
+        <li>En forelder sier til sitt barn at det er mange mennesker i verden som sulter, og derfor burde barnet spise maten sin</li>
+        <li>En aktivist sier at vår stolthet som nasjon står på spill, hvis vi ikke tar et klart standpunkt i saken om Israel</li>
+    </ul>
+</p>
+
+<p>Aristoteles sa at det er tre måter å overbevise mennesker på. Enten fordi de selv blir berørt, eller fordi de stoler på utsagnets kilde, eller fordi argumentet i seg selv er logisk overbevisende. En av grunnene til at det er nyttig å kjenne til de klassiske overbevisningsformene, er at vi blir mer bevisste på hvilken type argumentasjon som brukes, og det blir lettere å vurdere i hvilken grad man kan gå god for den.</p>
+
+<p>Her kommer en utfordring til leseren: neste gang du er vitne til en politisk debatt, kan du prøve å kategorisere argumentene som etos, logos eller patos. Kanskje passer noen av argumentene til flere merkelapper?</p>
+
+<p><i>PS: Noen inkluderer også et fjerde retorisk element, “kairos”. Kairos dreier seg om riktig tid og sted å levere sin argumentasjon - et slags metaelement. Men det får eventuelt bli et eget innlegg.</i></p>
+`
+
+export default {
+    title,
+    publishedDate: "16. august 2025",
+    publishedTime: "12.46",
+    lastUpdatedDate: "",
+    lastUpdatedTime: "",
+    slugs: ["de-tre-klassiske-overbevisningsformene"],
+    tags: ["Filosofi"],
+    bodyText
+}
