fe75f85ef0086863f864c8bbc2102456676c18f1
Erling <131489541+Monticulos@users.noreply.github.com>
2025-08-17 10:52:35 +0200

    turn sources from map to POJO

--- entries/index.js
+++ entries/index.js
@@ -1,1 +1,39 @@
-export const entryFileNames = ["2025-08-16.js","2025-08-09.js"]
+export const entries = [
+  {
+    "title": "De tre klassiske overbevisningsformene",
+    "publishedDate": "16. august 2025",
+    "publishedTime": "12.46",
+    "lastUpdatedDate": "",
+    "lastUpdatedTime": "",
+    "slugs": [
+      "de-tre-klassiske-overbevisningsformene"
+    ],
+    "tags": [
+      "Filosofi"
+    ],
+    "bodyText": "\n<p>Det er til stadighet noen som forsøker å overtale oss. Det kan være en kollega som vil at du skal gjøre noe for dem, reklameannonser i feeden din, eller noen du bor med som begrunner hvorfor de ikke har gjort sin del av husarbeidet. Vi rettferdiggjør også våre egne handlinger når vi tenker tilbake til dem, i en form for indre overtalelse. Siden påvirkningsforsøkene kommer på løpende bånd, er det verdt å bli bedre kjent med deres form og virkning.</p>\n\n<p>Retorikken - kunsten å overbevise - har røtter tilbake til Aristoteles, som levde i Hellas i det fjerde århundret før vår tidsregning. I moderne tid brukes hans lære som grunnlag i retorikkemner på skoler og universiteter. Han beskrev de tre retoriske virkemidlene <i>etos</i>, <i>logos</i> og <i>patos</i>, som berører henholdsvis karakter, fornuft og følelser.</p>\n\n<h2>Etos - appell til troverdighet</h2>\n\n<p>Ordet <i>etos</i> kommer fra gresk, og kan oversettes direkte til “karakter”, og handler om talerens troverdighet og autoritet. Det dreier seg om å overbevise lytteren om hvorfor hun skal høre på akkurat deg.</p>\n\n<p>For å styrke sin etos kan taleren nevne kvalifikasjoner som relevant utdanning og erfaring. Riktig talemåte og antrekk med hensyn til publikumet er også med på å styrke troverdigheten.</p>\n\n<p>Eksempler på etos:</p>\n<ul>\n    <li>En politiker som nevner hvilke bedrifter hun har besøkt, for å vise til god kontakt med folket</li>\n    <li>En produsent av eksklusive biler som bruker kjente sportsutøvere i reklamen sin, for å bygge en assosiasjon mellom utøverens karakter og bilmerket</li>\n    <li>En dresskledd revisor med masterutdanning, 20 års arbeidserfaring og tre bokutgivelser vil ha sterk etos på en økonomikonferanse, men samme person vil kanskje ikke stå like sterkt på et avslappet surfetreff</li>\n</ul>\n\n<h2>Logos - appell til fornuft</h2>\n\n<p>Det greske ordet <i>logos</i> kan oversettes direkte til “fornuft”, og er nært beslektet med ordet “logikk”. Denne overbevisningsformen handler om å oppgi fakta og logiske resonnementer som underbygger saken. Ved å vise til sterke, konkrete bevis for et synspunkt, vil argumentasjonen bli mer overbevisende. </p>\n\n<p>Eksempler på logos:</p>\n<ul>\n    <li>En bedriftsleder viser til svake årsresultater for å rettferdiggjøre en oppsigelse</li>\n    <li>En klimaaktivist peker på sammenhengen mellom drivhusgasser og temperaturøkning, og utleder at vi må bremse utslippene for å bremse temperaturøkning</li>\n    <li>En politiker viser til et naboland som innførte en utdanningsreform som ledet til forbedrede skoleresultater, og at vi derfor kan forvente lignende resultater</li>\n</ul>\n\n<h2>Etos - appell til følelser</h2>\n\n<p>Ordet <i>patos</i> kommer, som du sikkert har gjettet, også fra gresk. Patos kan oversettes til “følelse” og er beslektet med ord som “sympati” og “empati”. Denne overbevisningsformen har som mål å påvirke tilhørerne emosjonelt. Vi mennesker påvirkes ofte mer av følelser enn fakta, derfor er dette en verdifull overbevisningsform å kjenne til.</p>\n\n<p>Eksempler på patos:</p>\n<ul>\n    <li>En politiker råder til å ikke stemme på sin motstander, fordi vi vil gå en skremmende fremtid i møte</li>\n    <li>En forelder sier til sitt barn at det er mange mennesker i verden som sulter, og derfor burde barnet spise maten sin</li>\n    <li>En aktivist sier at vår stolthet som nasjon står på spill, hvis vi ikke tar et klart standpunkt i saken om Israel</li>\n</ul>\n\n<p>Aristoteles sa at det er tre måter å overbevise mennesker på. Enten fordi de selv blir berørt, eller fordi de stoler på utsagnets kilde, eller fordi argumentet i seg selv er logisk overbevisende. En av grunnene til at det er nyttig å kjenne til de klassiske overbevisningsformene, er at vi blir mer bevisste på hvilken type argumentasjon som brukes, og det blir lettere å vurdere i hvilken grad man kan gå god for den.</p>\n\n<p>Her kommer en utfordring til leseren: neste gang du er vitne til en politisk debatt, kan du prøve å kategorisere argumentene som etos, logos eller patos. Kanskje passer noen av argumentene til flere merkelapper?</p>\n\n<p><i>PS: Noen inkluderer også et fjerde retorisk element, “kairos”. Kairos dreier seg om riktig tid og sted å levere sin argumentasjon - et slags metaelement. Men det får eventuelt bli et eget innlegg.</i></p>\n",
+    "sources": {
+      "Retorikk (snl.no)": "https://snl.no/retorikk",
+      "Aristoteles (snl.no)": "https://snl.no/Aristoteles",
+      "What Are Ethos, Pathos, & Logos? (thesaurus.com)": "https://www.thesaurus.com/e/writing/ethos-pathos-logos/",
+      "Aristotle's Rhetoric (stanford.edu)": "https://plato.stanford.edu/entries/aristotle-rhetoric/#ThreMeanPers"
+    }
+  },
+  {
+    "title": "Velkommen til bloggen",
+    "publishedDate": "9. august 2025",
+    "publishedTime": "07.04",
+    "lastUpdatedDate": "",
+    "lastUpdatedTime": "",
+    "slugs": [
+      "velkommen-til-bloggen",
+      "velkommen"
+    ],
+    "tags": [
+      "Bloggen",
+      "Programmering"
+    ],
+    "bodyText": "\n<p>Velkommen til Bloggen - et prosjekt laget med Web Components. Den har to sider, visning av blogginnlegg og arkivet. I fremtiden kommer jeg også til å lage en egen side med emner.</p>\n\n<p>Denne bloggen oppstod av flere årsaker. Jeg har en kollega som snakket om Web Components og sa at det hadde kommet ganske langt. Da ble jeg nysgjerrig på hva det var for noe, og hvordan det er å bruke i forhold til React. Jeg er også interessert i hva rammeverk som React gjør for oss, og da er det greit å se hvordan ting fungerer uten rammeverk. I tillegg hadde vi en episode på jobb, da en NPM-avhengighet ble flagget som ondsinnet kode og tatt ned midlertidig, som gjorde meg interessert i å prøve å bygge noe uten avhengigheter.</p>\n",
+    "sources": null
+  }
+]
--- todo.md
+++ todo.md
@@ -1,4 +1,5 @@
 # To do
 * Lage byggesteg slik at hver side kan få et eget html-dokument (SEO)?
+* Bruke linter
 * Søkeboks på arkiv-siden
 * Lese https://kinsta.com/blog/web-components/
